8da53bb46aab70cedf191406ef1d1bcda61bb84b
Christian Schwartz <christian@schwartz-raum.de>
2011-08-10 16:04:59 +0200

    Basic functionality and tests

--- lib/tuplelist.rb
+++ lib/tuplelist.rb
@@ -1,5 +1,28 @@
 class TupleList
-  def initialize(required_keys)
+  attr_reader :keys
 
+  include Enumerable
+
+  def initialize(required_keys)
+    @keys = required_keys
+    @current_tuple = {}
+    @tuple_list = [@current_tuple] 
+  end
+
+  def []=(key, value)
+    raise "Unspecified key '#{key}' entered" unless @keys.include? key
+    @current_tuple[key] = value 
+  end
+
+  def next
+    raise "No keys for #{@current_tuple.keys & @keys} specified" unless @current_tuple.length == keys.length
+    @current_tuple = {}
+    @tuple_list << @current_tuple
+  end
+
+  def each
+    @tuple_list.each do |tuple_hash|
+      yield tuple_hash.values_at(*@keys)
+    end
   end
 end
--- test/test_tuplelist.rb
+++ test/test_tuplelist.rb
@@ -1,7 +1,56 @@
 require 'helper'
 
 class TestTuplelist < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_add_tuple_with_missing_values
+    tupleList = TupleList.new [:foo, :bar]
+    tupleList[:foo] = "foo"
+    assert_raises RuntimeError, 'No values for [:bar] specified' do
+      tupleList.next
+    end
+  end
+
+  def test_add_unspecified_key_to_tuple
+    tupleList = TupleList.new [:foo]
+    assert_raises RuntimeError, "Unspecified key 'bar' entered" do
+      tupleList[:bar] = "bar"
+    end
+  end
+
+  def test_for_each_tuple_if_next_is_called_after_last_row
+    tupleList = TupleList.new [:foo, :bar]
+    tupleList[:foo] = "foo-1"
+    tupleList[:bar] = "bar-1"
+    tupleList.next
+    tupleList[:foo] = "foo-2"
+    tupleList[:bar] = "bar-2"
+    tupleList.next
+
+    assert_each tupleList, [["foo-1", "bar-1"], ["foo-2", "bar-2"]]
+  end
+
+  def test_for_each_tuple_if_next_is_not_called_after_last_row
+    tupleList = TupleList.new [:foo, :bar]
+    tupleList[:foo] = "foo-1"
+    tupleList[:bar] = "bar-1"
+    tupleList.next
+    tupleList[:foo] = "foo-2"
+    tupleList[:bar] = "bar-2"
+
+    assert_each tupleList, [["foo-1", "bar-1"], ["foo-2", "bar-2"]]
+  end
+
+  def test_get_keys
+    keys = [:foo, :bar]
+    tupleList = TupleList.new keys
+    assert_equal keys, tupleList.keys
+  end
+
+  def assert_each(expected_enumerable, actual_enumerable, message = "Assertion failed")
+    error_messages = []
+    actual_enumerable.zip(expected_enumerable) do |actual_value, expected_value|
+      error_messages << "'#{actual_value.to_s} does not match '#{expected_value.to_s}'" unless actual_value == expected_value
+    end
+
+    raise MiniTest::Assertion.new("#{message}: #{error_messages}") unless error_messages.empty?
   end
 end
